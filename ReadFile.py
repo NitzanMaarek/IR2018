dbda455497eaae0e46bd55318611ba1085c7df5e
chenyanai <chenyanai@gmail.com>
2018-11-03 19:05:56 +0200

    Added Document Class in the ReadFile file, fixed a bug in the testing code

--- ReadFile.py
+++ ReadFile.py
@@ -30,5 +30,37 @@ class ReadFile:
                 job = self.pool.apply_async(self.read_file, filename)
                 self.jobs.append(job)
 
-    # def read_file(self, file_path):
+    def read_file(self, file_path):
+        with open(file_path) as file:
+            doc = []
+            for line in file:
+                if "<DOC>" in line:
+                    line
+                    doc = []
+                elif "</DOC>" in line:
+                    job = self.pool.apply_async(Document.__init__, doc) # Need to make sure this line works
+                    self.jobs.append(job)
+                else:
+                    doc.append(line)
 
+    # def parse_file(self, doc):
+
+
+class Document:
+    def __init__(self, data):
+        print(data)
+        self.doc_num = self.get_doc_parameter(data[1], 'DOCNO')
+        self.HT = self.get_doc_parameter(data[2], 'HT')
+
+
+    def get_doc_parameter(self, line, label):
+        print(line)
+        line.replace('<' + label + '>', '')
+        line.replace('</' + label + '>', '')
+        line.replace(' ', '')
+        print(line)
+        return line
+
+if __name__ == '__main__':
+    file = ReadFile('C:\\Chen\\BGU\\2019\\2018 - Semester A\\3. Information Retrival\\Engine\\test directory')
+    file.run_file_reader()
