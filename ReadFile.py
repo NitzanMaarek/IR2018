71019aff7a4b5faecbd373683d87d84316d8feaa
chenyanai <chenyanai@gmail.com>
2018-11-09 16:24:51 +0200

    Fixing multiproessing bugs

--- ReadFile.py
+++ ReadFile.py
@@ -5,43 +5,46 @@ import multiprocessing as mp
 import datetime
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import *
-from collections import Counter
-import itertools
+import json
 
 class ReadFile:
-    def __init__(self, directory):
+    manager = ''
+    q = ''
+    pool = ''
+    jobs = []
+
+    def __init__(self, directory, manager, q, pool):
         self.directory = directory
-        self.manager = mp.Manager()
-        self.q = self.manager.Queue()
-        self.pool = mp.Pool(mp.cpu_count() + 2)
-        self.jobs = []
+        ReadFile.manager = manager
+        ReadFile.q = q
+        ReadFile.pool = pool
+        # self.manager = mp.Manager()
+        # self.q = self.manager.Queue()
+        # self.pool = mp.Pool(mp.cpu_count() + 2)
+        # self.jobs = []
         self.multiprocess = True
 
     def run_file_reader(self):
         start_time = datetime.datetime.now()
         self.read_directory(self.directory)
-        self.q.put(['kill'])
-        self.pool.close()
-        self.pool.join()
+        # ReadFile.q.put(['kill'])
+        # ReadFile.pool.close()
+        ReadFile.pool.join()
         finish_time = datetime.datetime.now()
         print(finish_time - start_time)
 
     def read_directory(self, directory):
-        for filename in os.listdir(directory):
-            path = directory + '\\' + filename
-            if os.path.isdir(path):
+        for root, dirs, files in os.walk(r'C:\Chen\BGU\2019\2018 - Semester A\3. Information Retrival\Engine\corpus'):
+            for file in files:
+                # path = directory + '\\' + file + '\\' + file
+                path = os.path.join(root, file)
+                print(path)
                 if self.multiprocess:
-                    print('Directory: ' + path)
-                    job = self.pool.apply_async(self.read_directory, path)
-                    self.jobs.append(job)
-                else:
-                    self.read_directory(path)
-            else:
-                if self.multiprocess:
-                    job = self.pool.apply_async(self.read_file, path)
-                    self.jobs.append(job)
+                    job = ReadFile.pool.apply_async(self.read_file, path)
+                    ReadFile.jobs.append(job)
                 else:
                     self.read_file(path)
+        ReadFile.pool.join()
 
     def read_file(self, file_path):
         print('File: ' + file_path)
@@ -52,13 +55,14 @@ class ReadFile:
                 start = content.find('<DOC>') + 5
                 finish = content.find('</DOC>')
                 doc = content[start:finish]
+                print(doc)
                 if self.multiprocess:
-                    job = self.pool.apply_async(Document, doc)  # Need to make sure this line works
-                    self.jobs.append(job)
+                    job = ReadFile.pool.apply_async(ReadFile.Document, doc)  # Need to make sure this line works
+                    ReadFile.jobs.append(job)
                 else:
                     Document(doc)
-                content = content[finish:]
-
+                content = content[finish + 6:]
+        ReadFile.pool.join()
 
 class Document:
     # TODO: need to check if in every document those tags are in the same position
@@ -68,8 +72,14 @@ class Document:
         self.doc_num = self.get_doc_param('DOCNO') # in the same location
         self.HT = self.get_doc_param('HT') # in the same location
         self.set_text_full_text()
-        print(self.text)
-
+        # print(self.text)
+        dict_to_json = {}
+        dict_to_json['doc_num'] = self.doc_num
+        dict_to_json['HT'] = self.HT
+        dict_to_json['orig_data'] = self.orig_data
+        print(dict_to_json)
+        with open('jsons\\' + self.doc_num + 'txt', 'w') as outfile:
+            json.dump(dict_to_json, outfile)
 
     def get_doc_parameter(self, line, label):
         start_label = '<' + label + '>'
@@ -113,9 +123,11 @@ class Document:
             self.terms = set(word_tokenize(self.text))
 
 if __name__ == '__main__':
-    file = ReadFile(r'C:\Chen\BGU\2019\2018 - Semester A\3. Information Retrival\Engine\corpus')
-    file.multiprocess = False
-    file.read_directory('C:\\Chen\\BGU\\2019\\2018 - Semester A\\3. Information Retrival\\Engine\\corpus\\FB396001')
-    # file.run_file_reader()
-
+    manager = mp.Manager()
+    q = manager.Queue()
+    pool = mp.Pool()
+    file = ReadFile(r'C:\Chen\BGU\2019\2018 - Semester A\3. Information Retrival\Engine\corpus', manager, q, pool)
+    file.multiprocess = True
+    # file.read_directory('C:\\Chen\\BGU\\2019\\2018 - Semester A\\3. Information Retrival\\Engine\\corpus\\FB396001')
+    file.run_file_reader()
     # Counter([stemmer.stem(word) for word in word_tokenize(data)])
