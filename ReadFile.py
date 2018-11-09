f413f0d9451626a5655f0aed2e62e1c2aa05d9ad
chenyanai <chenyanai@gmail.com>
2018-11-09 14:32:55 +0200

    Changed ReadFile to read all the document text as one string and fixed some bugs in the multiprocessing

--- ReadFile.py
+++ ReadFile.py
@@ -8,7 +8,6 @@ from nltk.stem.porter import *
 from collections import Counter
 import itertools
 
-
 class ReadFile:
     def __init__(self, directory):
         self.directory = directory
@@ -16,7 +15,7 @@ class ReadFile:
         self.q = self.manager.Queue()
         self.pool = mp.Pool(mp.cpu_count() + 2)
         self.jobs = []
-        self.multiprocess = False
+        self.multiprocess = True
 
     def run_file_reader(self):
         start_time = datetime.datetime.now()
@@ -29,67 +28,46 @@ class ReadFile:
 
     def read_directory(self, directory):
         for filename in os.listdir(directory):
-            if os.path.isdir(filename):
+            path = directory + '\\' + filename
+            if os.path.isdir(path):
                 if self.multiprocess:
-                    job = self.pool.apply_async(self.read_directory, directory + '\\' + filename)
+                    print('Directory: ' + path)
+                    job = self.pool.apply_async(self.read_directory, path)
                     self.jobs.append(job)
                 else:
-                    self.read_directory(directory + '\\' + filename)
+                    self.read_directory(path)
             else:
                 if self.multiprocess:
-                    job = self.pool.apply_async(self.read_file, directory + '\\' + filename)
+                    job = self.pool.apply_async(self.read_file, path)
                     self.jobs.append(job)
                 else:
-                    self.read_file(directory + '\\' + filename)
+                    self.read_file(path)
 
     def read_file(self, file_path):
-        with open(file_path) as file:
-            doc = []
-            for line in file:
-                if "<DOC>" in line:
-                    doc = []
-                elif "</DOC>" in line:
-                    if self.multiprocess:
-                        job = self.pool.apply_async(Document.__init__, doc) # Need to make sure this line works
-                        self.jobs.append(job)
-                    else:
-                        Document(doc)
-                elif line is not '\n':
-                    line = line.replace('\n', '')
-                    doc.append(line)
-
-    # def parse_file(self, doc):
+        print('File: ' + file_path)
+        with open(file_path, 'r') as content_file:
+            content = content_file.read()
+            content = content.replace('\n', '')
+            while(content is not ''):
+                start = content.find('<DOC>') + 5
+                finish = content.find('</DOC>')
+                doc = content[start:finish]
+                if self.multiprocess:
+                    job = self.pool.apply_async(Document, doc)  # Need to make sure this line works
+                    self.jobs.append(job)
+                else:
+                    Document(doc)
+                content = content[finish:]
 
 
 class Document:
     # TODO: need to check if in every document those tags are in the same position
-    def __init__(self, data, stem):
-        self.stem = stem
-        self.doc_num = self.get_doc_parameter(data[0], 'DOCNO') # in the same location
-        self.HT = self.get_doc_parameter(data[1], 'HT') # in the same location
-
-        start = -1
-        finish = -1
-        for i in range(0, len(data)):
-            if '<DATE1>' in data[i]:
-                self.date = self.get_doc_parameter(data[i], 'DATE1')
-            elif '<TI>' in data[i]:
-                self.title = self.get_doc_parameter(data[i], 'TI')
-            if (start == -1 and '<TEXT>' in data[i]) or ('[Text]' in data[i]):
-                start = i + 1
-            if '</TEXT>' in data[i]:
-                finish = i
-                break
-        data[i] = data[i].replace('[Text]', '')
-        data[i] = data[i].replace('<TEXT>', '')
-        self.text = list(itertools.chain.from_iterable(data[start:finish]))
-
-
-        # self.AU = self.get_doc_parameter(data[3], 'AU') # not in the same location
-        # self.Header = data[4] # not in the same location
-        # self.date = self.get_doc_parameter(data[5], 'DATE1') # not in the same location
-        # self.text = ''
-        # self.set_text(data) # Not actual text, need to add language as parameter
+    def __init__(self, data):
+        self.stem = PorterStemmer()
+        self.orig_data = data
+        self.doc_num = self.get_doc_param('DOCNO') # in the same location
+        self.HT = self.get_doc_param('HT') # in the same location
+        self.set_text_full_text()
         print(self.text)
 
 
@@ -112,6 +90,22 @@ class Document:
         data[i] = data[i].replace('<TEXT>', '')
         self.text = data[start:finish]
 
+    def set_text_full_text(self):
+        finish = self.orig_data.find('</TEXT>')
+        start = self.orig_data.find('<TEXT>') + 6
+        start_brackets = self.orig_data.find('[Text]') + 6
+        if start > start_brackets:
+            self.text = self.orig_data[start:finish]
+        else:
+            self.text = self.orig_data[start_brackets:finish]
+
+    def get_doc_param(self, label):
+        start_label = '<' + label + '>'
+        start = self.orig_data.find(start_label)
+        finish = self.orig_data.find('</' + label + '>')
+        return self.orig_data[start + len(start_label):finish]
+
+
     def create_terms(self):
         if self.stem:
             self.terms = set(word_tokenize(self.text))
@@ -119,17 +113,9 @@ class Document:
             self.terms = set(word_tokenize(self.text))
 
 if __name__ == '__main__':
-    # list = []
-    # with open('C:\\Chen\\BGU\\2019\\2018 - Semester A\\3. Information Retrival\\Engine\\test directory\\testfile.txt') as file_test:
-    #     for line in fiC>" in line:
-    #         #     print(line)
-    #         if line is not '\n':
-    #             line = line.replace('\n', '')
-    #             list.append(line)
-    # for line in list:le_test:
-    #         # if "<DO
-    #     print(list)
-    file = ReadFile('C:\\Chen\\BGU\\2019\\2018 - Semester A\\3. Information Retrival\\Engine\\test directory')
-    file.run_file_reader()
+    file = ReadFile(r'C:\Chen\BGU\2019\2018 - Semester A\3. Information Retrival\Engine\corpus')
+    file.multiprocess = False
+    file.read_directory('C:\\Chen\\BGU\\2019\\2018 - Semester A\\3. Information Retrival\\Engine\\corpus\\FB396001')
+    # file.run_file_reader()
 
     # Counter([stemmer.stem(word) for word in word_tokenize(data)])
