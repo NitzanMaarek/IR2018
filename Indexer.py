353b31c096f28b226f98b431440afc7fc74f290b
Nizzo <maarekn@post.bgu.ac.il>
2018-12-02 12:58:56 +0200

    Writes posting and dictionary with new lines
    Fixed seek value (pointer in dictionary)

--- Indexer.py
+++ Indexer.py
@@ -153,8 +153,8 @@ def merge_chunks(chunks_directory, num_of_chunks, stem_flag):
                 term_documents_attributes = term_record.create_string_from_doc_dictionary() #TODO: Need to add pointer to documents to attributes.
                 str_for_posting = ''.join([term, ' ', term_documents_attributes, '\n'])
                 #string is: term <doc_id tf first_position_in_doc doc_pointer> <doc_id tf first_position_in_doc doc_pointer>...
-                seek_offset += len(str_for_posting)
                 str_for_dictionary = ''.join([term, ' <', str(merged_prefix_dictionary[term].df), ' ', posting_file_name, ' ', str(seek_offset), '>\n'])
+                seek_offset += len(str_for_posting)
                 #string is term df posting_file_name seek_position(pointer to line of term)
                 d.write(str_for_dictionary)
                 tp.write(str_for_posting)
