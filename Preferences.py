4ce542ba3b74675d0b1b84d933ddef25b150d77f
chenyanai <chenyanai@gmail.com>
2018-12-03 14:38:22 +0200

    Preferences class which all the other class takes global arguments from

---
+++ Preferences.py
@@ -0,0 +1,9 @@
+
+# Project directory
+main_directory = 'C:\\Chen\\BGU\\2019\\2018 - Semester A\\3. Information Retrival\\Engine\\test directory\\created files\\'
+
+# How many posting files to write in each iteration
+posting_files_per_batch = 10
+
+# If we stem or not
+stem = False
